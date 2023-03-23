f3469191c71b3edcbe93de8cce4147120e5e0a57
Caden Kroonenberg <cskroonenberg@gmail.com>
2023-03-22 23:03:29 -0500

    Update build script to source ros and install cadd-e executable

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 # Build interfaces
+source /opt/ros/foxy/setup.bash
+
 cd cadd_e_interfaces
 colcon build --packages-select cadd_e_interfaces
 . install/setup.bash
 
 cd ..
 colcon build --packages-select cadd-e
+. install/setup.bash
