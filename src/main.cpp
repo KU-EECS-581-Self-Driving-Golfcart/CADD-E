dae364ad2d6a2711b438bdc4b5101c7a399f63a6
Caden Kroonenberg <cskroonenberg@gmail.com>
2023-03-22 23:33:46 -0500

    Add IMU subscriber to main control loop

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,18 @@
 #include <iostream>
 #include "rclcpp/rclcpp.hpp"
 #include "cadd_e_interface/msg/gps.hpp"
+#include "cadd_e_interface/msg/imu.hpp"
 
 #include <unistd.h>
 
 using std::placeholders::_1;
+
+float lat = 0.0;
+float lon = 0.0;
+float heading = 0.0;
+float lin_acc_x = 0.0;
+float lin_acc_y = 0.0;
+float lin_acc_z = 0.0;
 
 class GPSSubscriber : public rclcpp::Node
 {
@@ -17,24 +25,52 @@ public:
   GPSSubscriber()
   : Node("gps_subscriber")
   {
-    subscription_ = this->create_subscription<cadd_e_interface::msg::GPS>(          // CHANGE
-      "position", 10, std::bind(&GPSSubscriber::topic_callback, this, _1));
+    subscription_ = this->create_subscription<cadd_e_interface::msg::GPS>(
+      "position", 1, std::bind(&GPSSubscriber::topic_callback, this, _1));
   }
 
 private:
-  void topic_callback(const cadd_e_interface::msg::GPS::SharedPtr msg) const       // CHANGE
+  void topic_callback(const cadd_e_interface::msg::GPS::SharedPtr msg) const
   {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%f, %f'", msg->lat, msg->lon);              // CHANGE
+    // RCLCPP_INFO(this->get_logger(), "I heard: '%f, %f'", msg->lat, msg->lon);
+    lat = msg->lat;
+    lon = msg->lon;
   }
-  rclcpp::Subscription<cadd_e_interface::msg::GPS>::SharedPtr subscription_;       // CHANGE
+  rclcpp::Subscription<cadd_e_interface::msg::GPS>::SharedPtr subscription_;
+};
+
+class IMUSubscriber : public rclcpp::Node
+{
+public:
+  IMUSubscriber()
+  : Node("imu_subscriber")
+  {
+    subscription_ = this->create_subscription<cadd_e_interface::msg::IMU>(
+      "telemetry", 1, std::bind(&IMUSubscriber::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const cadd_e_interface::msg::IMU::SharedPtr msg) const
+  {
+    // RCLCPP_INFO(this->get_logger(), "I heard: '%f, %f'", msg->lat, msg->lon);
+    heading = msg->heading;
+    lin_acc_x = msg->lin_acc_x;
+    lin_acc_y = msg->lin_acc_y;
+    lin_acc_z = msg->lin_acc_z;
+  }
+  rclcpp::Subscription<cadd_e_interface::msg::IMU>::SharedPtr subscription_;
 };
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
 
-    rclcpp::Node::SharedPtr sub_node = std::make_shared<GPSSubscriber>();
+    rclcpp::Node::SharedPtr gps_sub_node = std::make_shared<GPSSubscriber>();
+    rclcpp::Node::SharedPtr imu_sub_node = std::make_shared<IMUSubscriber>();
     rclcpp::executors::SingleThreadedExecutor executor;
-    executor.add_node(sub_node);
+
+    executor.add_node(gps_sub_node);
+    executor.add_node(imu_sub_node);
+
     std::cout << "initial spin...\n";
     executor.spin_once();
 
@@ -48,16 +84,17 @@ int main(int argc, char* argv[]) {
 
     int i = 0;
     while(rclcpp::ok()) {
-        std::cout << i << "\n";
-        //double gps_x = 0;   // TODO
-        //double gps_y = 0;   // TODO
+        std::cout << ++i << "\n";
         //int hole = 1;       // TODO
         //int loc = 0;        // TODO
 
-        //std::tie(routeX, routeY) = rp.ShortestRoute(gps_x, gps_y, hole, loc);
+        //std::tie(routeX, routeY) = rp.ShortestRoute(lat, lon, hole, loc);
 
         std::cout << "spinning...\n";
         executor.spin_once();
+
+        std::cout << "New coords: [" << lat << ", " << lon << "]\n";
+        std::cout << "New telemetry: [h: " << heading << "; l_a: " << lin_acc_x << ", " << lin_acc_y << ", " << lin_acc_z << "]\n";
 
         std::tie(routeX, routeY) = rp.m.ShortestRoute(7208400605, 7220557245); // Hole 1 black tee to hole 9 green
     
@@ -89,6 +126,8 @@ int main(int argc, char* argv[]) {
             std::cout << "no path found\n";
         }
         // TODO: Convert path to clothoid
+
+        sleep(1);
 
     }
     rclcpp::shutdown();
