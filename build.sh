ba3653adfdc15f5a4e626417c11e4cc1a1f6f37a
Caden Kroonenberg <kroonenberg@avinc.com>
2023-04-10 12:44:32 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 # Build interfaces
 source /opt/ros/foxy/setup.bash
 
+# Build sensor interface
 cd cadd_e_interface
 colcon build --packages-select cadd_e_interface
 . install/setup.bash
+cd ..
 
+# Build controller
 cd src/control
 colcon build
 . install/setup.bash
 cd .. && cd ..
 
-cd ..
+# Build CADD-E
 colcon build --packages-select cadd-e
 . install/setup.bash
