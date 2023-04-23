9b1f5d7e6e8e0c2ef0fbd4655fae2b8fa07636c9
Caden Kroonenberg <cskroonenberg@gmail.com>
2023-04-23 17:50:50 -0500

    Add Pi setup script

---
+++ scripts/pi/pi_setup.sh
@@ -0,0 +1,7 @@
+mkdir -p ros2_sensor_ws/src
+cd ros2_sensor_ws/src
+git clone https://github.com/KU-EECS-581-Self-Driving-Golfcart/cadd_e_interface
+git clone https://github.com/KU-EECS-581-Self-Driving-Golfcart/gps_pubsub
+git clone https://github.com/KU-EECS-581-Self-Driving-Golfcart/us_imu_pubsub
+
+sudo apt install libopencv-dev python3-opencv
